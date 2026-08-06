d5b37957a5baba65297493faab42adea147f9f7b
Vipin Mishra <vipinmishra2302@gmail.com>
2026-08-06 09:10:30 +0530

    add first file of dsa series

---
+++ video2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+//     cout<<"vipin mishra"<<endl;  // we use endl so that next print statement prints the statement in next line
+//     cout<<"vipin mishra hi hu bhai";
+
+string name = "animal"; // whenever we want to use only single character like 'a' then we should use only single line quote 
+cout<<"vipin\nmishra";
+return 0;
+}
+
+
+
