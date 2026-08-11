cc75add210ae8d9ea233d7f9d8c5260c1d2d2174
Vipin Mishra <vipinmishra2302@gmail.com>
2026-08-11 21:08:26 +0530

    add fibonacci series program

---
+++ functions/problems/fibonacci.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+int fibonacci(int n){
+    int a = 0;
+    int b = 1;
+    int sum = 0;
+
+    cout<<a<<endl;
+    cout<<b<<endl;
+
+    int i = 1;
+    while(i<=n){
+        sum = a+b;
+        cout<<sum<<endl;
+
+        a = b;
+        b = sum;
+        i++;
+    }
+
+
+}
+
+int main(){
+    int n;
+    cout<<"Enter the number: ";
+    cin>>n;
+    
+    fibonacci(n);
+    
+    return 0;
+}
+
+/*
+---------------------Output---------------------------------------
+
+Enter the number: 10
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+55
+89
+
+------------------------------------------------------------------
+*/
