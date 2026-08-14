9484e3657ebfa375cbed0762f112bc44f8f102ea
Vipin Mishra <vipinmishra2302@gmail.com>
2026-08-14 18:57:24 +0530

    program to pass an element by reference

---
+++ array/programs/pass_by_reference.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int changeArr(int arr[], int size){
+    cout<<"in function\n";
+    for(int i = 0;i<size;i++){
+        arr[i] = 2*arr[i];
+
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+
+
+}
+
+int main(){
+    int arr[] = {1,2,3};
+    changeArr(arr,3);
+
+    cout<<"in  main\n";
+    for(int i = 0; i<3;i++){
+        cout<<arr[i]<<" ";
+
+    }
+
+    cout<<endl;
+
+    return 0;
+}
