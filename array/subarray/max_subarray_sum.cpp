cc235f651412a161743abd4639516e56047b6caa
Vipin Mishra <vipinmishra2302@gmail.com>
2026-08-17 23:31:43 +0530

    add max subarray sum program

---
+++ array/subarray/max_subarray_sum.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+   
+
+    int arr[] = {2,3,5,7,8,5,3};
+    int n = sizeof(arr)/sizeof(int); 
+    
+    int max_sum = INT16_MIN;
+   
+    
+    for(int i = 0; i<n;i++){
+        int sum = 0;
+        for(int j = i; j<n;j++){
+
+            sum +=arr[j];
+            max_sum = max(sum,max_sum);
+        }
+        
+    }
+
+    cout<<"Max sum = "<<max_sum<<endl;
+    
+    return 0;
+}
+
+/*
+---------------Output-----------------------
+
+2 23 235 2357 23578 235785 2357853 
+3 35 357 3578 35785 357853 
+5 57 578 5785 57853 
+7 78 785 7853 
+8 85 853 
+5 53 
+3 
+
+--------------------------------------------
+*/
