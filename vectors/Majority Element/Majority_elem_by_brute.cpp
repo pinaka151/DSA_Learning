bf3aa027d799570a115b446f6fc8277be5d31414
Vipin Mishra <vipinmishra2302@gmail.com>
2026-08-22 13:20:57 +0530

    solve majority element question by brute force  method

---
+++ vectors/Majority Element/Majority_elem_by_brute.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int Majority_elem(vector <int> vect,int size){
+    int count = 0;
+    int start =0;
+
+    for(int elem : vect){
+
+        for(int i = start; i<size;i++){
+            if(vect[i]==elem){
+                count++;
+            }
+        }
+
+        start++;
+
+        if(count>size/2){
+            return elem;
+        }
+
+        count = 0;
+    }
+}
+
+int main(){
+    vector <int> vect = {1,2,1,1,4,4,4,4,4};
+    int size = vect.size();
+
+    int maj_elem = Majority_elem(vect,size);
+
+    cout<<maj_elem<<" is the majority element from the given vector"<<endl;
+
+
+    return 0;
+
+}
+
+
+/*
+---------------Output-----------------------
+
+4 is the majority element from the given array
+
+--------------------------------------------
+*/
