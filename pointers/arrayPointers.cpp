ebdd0209bfb0385525a03e49cf7b8dc355dfcd9d
Vipin Mishra <vipinmishra2302@gmail.com>
2026-08-31 23:41:37 +0530

    add arrayPointers concept

---
+++ pointers/arrayPointers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+int main(){
+int arr[] = {1,2,3,4,5};
+    cout<<*arr<<endl;
+    return 0;
+}
+
+/*
+---------------Output-----------------------
+
+1
+--------------------------------------------
+*/
