85748b6f0bb4096c1dd5616a9232beb7fbcfad52
Vipin Mishra <vipinmishra2302@gmail.com>
2026-09-06 23:34:45 +0530

    add program to solve book allocation problem

---
+++ vectors/Book Allocation Problem21/book_alloc.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+bool isValid(vector <int> &arr, int n, int  m, int maxAllowedPage){
+    int students = 1, pages = 0;
+    for(int i = 0; i<n; i++){
+        if(arr[i]>maxAllowedPage){
+            return false;
+        }
+        if(pages + arr[i]<=maxAllowedPage){
+            pages+=arr[i];
+
+        }
+        else{
+            students++;
+            pages = arr[i];
+        }
+    }
+    return students > m ? false:true;
+}
+
+int allocateBooks(vector<int> &arr, int n, int m){
+
+    if(m>n){
+        return -1;
+    }
+    int  sum = 0;
+    int size= arr.size();
+    for(int  i = 0; i<size;i++){
+        sum+=arr[i];
+    }
+
+    int str = 0;
+    int end = sum;
+    int ans = -1;
+    while(str<=end){
+        int  mid = str + (end-str)/2;
+        if(isValid(arr,n,m,mid)){
+            ans = mid;
+            end = mid -1;
+        }
+        else{
+            str = mid+1;
+        }
+    }
+
+
+return ans;
+}
+
+
+int main(){
+    vector<int> vect = {2,1,3,4};
+    int n = 4;
+    int m = 2;
+
+    cout<<allocateBooks(vect,n,m)<<endl;
+    
+}
